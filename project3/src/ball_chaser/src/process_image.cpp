dc9d8d3181e8ea8b76fdf08885887f0d0b1808e2
yuwei-wu <yuweiwu20001@outlook.com>
2020-05-24 02:39:25 -0400

    update

--- project3/src/ball_chaser/src/process_image.cpp
+++ project3/src/ball_chaser/src/process_image.cpp
@@ -26,43 +26,46 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-    ROS_INFO("Process images");
+
     int white_pixel = 255;
+
     // Loop through each pixel in the image and check if there's a bright white one
-
-
-    int locationy = -1;
-    for (int i = 0; i<  img.height * img.step  ; i++) {
-        if (img.data[i] == white_pixel ) {
-            locationy = i % img.step;
-            break;
-        }
-   
-   }
-   ROS_INFO_STREAM(locationy);
-   ROS_INFO_STREAM(img.step);
-   ROS_INFO("Process images");
     // Then, identify if this pixel falls in the left, mid, or right side of the image
-
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
+    // Request a stop when there's no white ball seen by the camera
+
+    int move_left = 0;
+    int move_right = 0;
+    int move_forward = 0;
+
+
+    for (int i = 0; i < img.height * img.step; i+=3) {
+        int location = i % (img.width * 3) / 3;
+    	if(img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel){
+          if(location <= 300){
+            move_left +=1;
+          }else if(location >= 600){ move_right +=1;
+          }else{
+            move_forward +=1;
+          }
+        }
+  }
 
 
     double lin_x = 0;
-    double ang_z = 0; // pase info to the robot
-
-
-    if ( locationy <= img.step/3 ) {ang_z = 0.5;
-
-   }else if ( locationy >= img.step *2.0/3 ) {ang_z = -0.5;}
-
-    else if( locationy != -1){
+    double ang_z = 0;
+    if(move_left > move_right && move_left > move_forward){
+      ang_z = -0.5;
+    }else if(move_right > move_left && move_right > move_forward){
+      ang_z = 0.5;
+    }else if(move_forward > move_left && move_forward > move_right){
       lin_x = 0.5;
     }
 
 
-     
-    // Request a stop when there's no white ball seen by the camera
-    drive_robot(lin_x, ang_z);   //0 0 
+
+
+    drive_robot(lin_x, ang_z);
 
 }
 
